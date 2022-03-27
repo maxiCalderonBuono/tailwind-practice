8e37e9adbb5f9a18ed1d587ebe5543cf8d3405bf
Maximiliano <calderonmaxi@outlook.com>
2022-03-26 21:37:37 -0300

    feat: responsive design

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {pokelist} from "./data";
 
 function App() {
   return (
-    <div className="text-white bg-zinc-800 h-screen">
-      <h1 className="text-center py-4 text-2xl font-bold ">
+    <div className= "h-full text-white bg-zinc-800">
+      <h1 className="py-4 text-2xl font-bold text-center ">
         Lista Simple
       </h1>
       <PokemonList list={pokelist}/>
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from "react";
 
-const PokemonList = ({list}) => {
+const PokemonList = ({ list }) => {
+  const switchColor = (type) => {
+    switch (type) {
+      case "agua":
+        return "bg-sky-500";
+      case "planta":
+        return "bg-green-500";
+      case "fuego":
+        return "bg-orange-500";
+      default:
+        return "bg-black";
+    }
+  };
+
   return (
-    <div className='flex flex-row justify-center flex-wrap md:w-4/12 mx-auto'>
-        {list.map((pokemon,index) => (
-            <div className= "flex flex-col w-40 h-40 bg-black m-2 rounded-lg text-white justify-center items-center" key= {index}>
-               <img className="w-24 h-24 object-contain"src={pokemon.url} alt={pokemon.name}/>
-               <p className='text-center'>{pokemon.name}</p>
-            </div>
-        ))}
+    <div className="flex flex-col flex-wrap justify-center mx-auto md:flex-row md:w-6/12">
+      {list.map((pokemon, index) => (
+        <div
+          className={`flex md:flex-col items-center justify-center md:w-40 h-40 m-2 relative cursor-pointer hover:scale-110 transition-all text-white rounded-lg ${switchColor(pokemon.types)}`}
+          key={index}
+        >
+          <img className="absolute w-10 h-10 md:h-6 md:w-6 top-2 right-2" src={pokemon.icon} alt= {pokemon.types}/>
+          <img
+            className="object-contain w-36 h-36 md:w-24 md:h-24"
+            src={pokemon.url}
+            alt={pokemon.name}
+          />
+          <p className="text-3xl font-bold text-center capitalize md:text-base">{pokemon.name}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default PokemonList
+export default PokemonList;
